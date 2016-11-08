8e9ae40baefc1f7333d1d307205dce2c3a271a77
powlo <paulcbackhouse@gmail.com>
2016-11-08 17:12:46 +0000

    Add facebook auth.

--- app/routes.js
+++ app/routes.js
@@ -53,6 +53,19 @@ module.exports = function(app, passport) {
     });
 
     // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook'));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));    
+
+    // =====================================
     // LOGOUT ==============================
     // =====================================
     app.get('/logout', function(req, res) {
